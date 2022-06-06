465fe8afff7073e088824ccc4e5b0fe139eb8b3e
Yasmin <yasmin.bunnie@gmail.com>
2022-06-06 23:10:57 +0100

    added the GET_ME query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,22 @@
 import { gql } from "@apollo/client";
 
+// Query built from TypeDefs Query me which asks for User
+// User can be found in mutations for when a User is created
 export const GET_ME = gql`
-query me {
-
-}
+  query GET_ME {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
 `;
